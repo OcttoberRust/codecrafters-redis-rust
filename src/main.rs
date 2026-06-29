529708af169a0b2fe7204143e026fdbbe57c6b5c
octtoberrust <octtoberrust@outlook.com>
2026-06-29 18:05:28 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                println!("+PONG\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
