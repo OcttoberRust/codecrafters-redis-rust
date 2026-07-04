b0f5c77a64da90f6f752830ee09ada7b2a0e97c6
octtoberrust <octtoberrust@outlook.com>
2026-07-04 12:36:19 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![allow(unused_imports)]
 use std::{io::{Read, Write}, net::TcpListener};
+use std::thread;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    
+    thread::spawn(move ||
+    {});
     
     for stream in listener.incoming() {
         match stream {
